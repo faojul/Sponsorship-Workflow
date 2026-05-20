8497639b4b9a44517a200f1cfe4457b4261fb7ef
faojul <faojulahsan@gmail.com>
2026-05-20 07:17:44 +0600

    feat: bind services & migrations

---
+++ backend/src/Sponsorship.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Sponsorship.Application.Abstractions;
+using Sponsorship.Application.Abstractions.Authentication;
+using Sponsorship.Application.Abstractions.Persistence;
+using Sponsorship.Infrastructure.Authentication;
+using Sponsorship.Infrastructure.Identity;
+using Sponsorship.Infrastructure.Persistence;
+using System.Text;
+
+namespace Sponsorship.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddHttpContextAccessor();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(
+                    configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IApplicationDbContext>(
+                provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddIdentityCore<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            ConfigureJWTToken(services, configuration);
+            services.AddAuthorization();
+
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+            services.AddScoped<IIdentityService, IdentityService>();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureJWTToken(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtOptions>(
+                configuration.GetSection(JwtOptions.SectionName));
+
+            var jwtOptions = configuration
+                .GetSection(JwtOptions.SectionName)
+                .Get<JwtOptions>()!;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+
+                options.DefaultChallengeScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidAudience = jwtOptions.Audience,
+
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(
+                                    jwtOptions.SecretKey))
+                    };
+            });
+
+            services.AddScoped<IJwtTokenGenerator,
+                JwtTokenGenerator>();
+
+            return services;
+        }
+    }
+}
