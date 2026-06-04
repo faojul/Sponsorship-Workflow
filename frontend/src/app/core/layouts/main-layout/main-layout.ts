a00305dd445949f29a4f384dccd21b8754e02d03
faojul <faojulahsan@gmail.com>
2026-06-04 22:55:42 +0600

    feat(ui): mobile responsiveness for table & card

--- frontend/src/app/core/layouts/main-layout/main-layout.ts
+++ frontend/src/app/core/layouts/main-layout/main-layout.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/auth';
 import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-layout',
@@ -13,13 +15,24 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatListModule,
     RouterModule,
-    MatButtonModule 
+    MatButtonModule,
+    MatIconModule 
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
 })
-export class MainLayout {
-  constructor(public authService: AuthService) {}
+export class MainLayout implements OnInit {
+  isMobile = false;
+  constructor(public authService: AuthService,
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe(['(max-width: 768px)']).subscribe((result) => {
+      this.isMobile = result.matches;
+      console.log('Breakpoint change detected. isMobile:', this.isMobile);
+    });
+  }
 
   logout(): void {
 
